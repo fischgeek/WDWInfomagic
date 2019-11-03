d1fa8749a25b9636707a45aa8f89bd4f6221fc16
Jeremiah Fischer <fischgeek@gmail.com>
2019-11-03 13:02:02 -0600

    quick

---
+++ WDWInfomagic/Models/Ride.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WDWInfomagic.Models
+{
+    public class Ride
+    {
+        public RideMeta meta { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public bool active { get; set; }
+        public decimal waitTime { get; set; }
+        public bool fastPass { get; set; }
+        public DateTime lastUpdate { get; set; }
+        public string status { get; set; }
+    }
+
+    public class RideMeta
+    {
+        public string area { get; set; }
+        public float longitude { get; set; }
+        public float latitude { get; set; }
+    }
+}
