efac3c2aa2727c12d7acf55ba54879e1b6258124
Jeremiah Fischer <fischgeek@gmail.com>
2019-11-22 11:42:49 -0600

    quick

---
+++ WDWInfomagic/Models/OpeningTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WDWInfomagic.Models
+{
+    public class OpeningTime
+    {
+        public DateTime date { get; set; }
+        public DateTime openingTime { get; set; }
+        public DateTime closingTime { get; set; }
+        public string type { get; set; }
+        public Special[] special { get; set; }
+    }
+
+    public class Special
+    {
+        public DateTime openingTime { get; set; }
+        public DateTime closingTime { get; set; }
+        public string type { get; set; }
+    }
+}
